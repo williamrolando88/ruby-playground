cbcf2785e5dd5945e0c2000facb30dc2dd281847
williamrolando88 <william.rolando88@hotmail.com>
2022-03-30 11:13:22 -0500

    Created: Scaffold for simple user and text submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :blogs
   get 'pages/hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # root "pages#hello"
+  root "blogs#index"
 end
